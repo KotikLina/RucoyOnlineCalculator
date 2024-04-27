89e70fff1734f96e37c1324ef144f08e8cf988c4
kotiklinok <kotiklinok@gmail.com>
2024-04-27 15:37:59 +0700

    Добавил временное общее хранилище.

--- TrainModel.py
+++ TrainModel.py
@@ -1,68 +1,12 @@
-import json
 import disnake
 import itertools
 from abc import ABC, abstractmethod
 from typing import Final, AsyncGenerator
 
-
-with open("Mobs_info.json", "r") as file:
-    data = json.load(file)
-
-mobs_info: list[dict] = data["mobs"]
-low_hp_mobs = []
-high_hp_mobs = []
-
-for mob in mobs_info:
-    if mob["health"] < 10000:
-        low_hp_mobs.append(mob)
-    else:
-        high_hp_mobs.append(mob)
+import temporary_common_storage
 
 
-class Context:
-    DEFAULT_MOB: Final[dict] = {"defense": 1000}
-    mob: dict
-
-    def __init__(self, mob: dict | None = None) -> None:
-        if mob is None:
-            self.mob = self.DEFAULT_MOB
-        else:
-            self.mob = mob
-
-
-class AbstractPerson(ABC):
-    lvl: int
-    stat: int
-    buffs: int
-    real_stat: int
-    weapon_atk: int
-
-    min_raw_damage: float
-    max_raw_damage: float
-    max_raw_crit_damage: float
-
-    min_damage: float
-    max_damage: float
-    max_crit_damage: float
-
-    normal_accuracy: int
-    crit_accuracy: int
-    accuracy: float
-
-    average_damage: int
-
-    ctx: Context
-
-    def __init__(self, lvl: int, stat: int, buffs: int, weapon_atk: int, ctx: Context) -> None:
-        self.lvl = lvl
-        self.stat = stat
-        self.buffs = buffs
-        self.weapon_atk = weapon_atk
-
-        self.ctx = ctx
-
-
-class Person(AbstractPerson):
+class Person(temporary_common_storage.AbstractPerson):
     @property
     def real_stat(self) -> int:
         return self.stat + self.buffs
@@ -123,7 +67,7 @@ class AbstractBattleModel(ABC):
     tickrate: int
     time: int
 
-    ctx: Context
+    ctx: temporary_common_storage.Context
 
     @abstractmethod
     def __init__(self, lvl: int, stat: int, buffs: int, weapon_atk: int) -> None:
@@ -139,7 +83,7 @@ class BattleModel(AbstractBattleModel):
     TRAIN_TICKRATE: Final[int] = 3600
 
     def __init__(self, lvl: int, stat: int, buffs: int, weapon_atk: int) -> None:
-        self.ctx = Context()
+        self.ctx = temporary_common_storage.Context()
         self.person = Person(lvl, stat, buffs, weapon_atk, ctx=self.ctx)
 
         self.trained_person = None
@@ -160,7 +104,7 @@ class BattleModel(AbstractBattleModel):
         self.frontier_group = None
 
         key = lambda x: (x["defense"])
-        iterator = itertools.groupby(sorted(low_hp_mobs, key=key, reverse=True), key=key)
+        iterator = itertools.groupby(sorted(temporary_common_storage.low_hp_mobs, key=key, reverse=True), key=key)
 
         prev = None
         for _, group in iterator:
@@ -189,7 +133,7 @@ class BattleModel(AbstractBattleModel):
             self.trained_stat = 0
             return
 
-        ctx = Context()
+        ctx = temporary_common_storage.Context()
         ctx.mob = self.frontier_group[0]
         self.trained_person = self.person.copy(ctx)
         self.trained_stat = 0
@@ -206,7 +150,7 @@ class BattleModel(AbstractBattleModel):
             return
 
         key = lambda x: (x["defense"])
-        iterator = itertools.groupby(sorted(mobs_info, key=key, reverse=True), key=key)
+        iterator = itertools.groupby(sorted(temporary_common_storage.mobs_info, key=key, reverse=True), key=key)
 
         for defense, group in iterator:
             group = tuple(group)
---
+++ temporary_common_storage.py
@@ -0,0 +1,58 @@
+import json
+
+
+with open("Mobs_info.json", "r") as file:
+    data = json.load(file)
+
+mobs_info: list[dict] = data["mobs"]
+low_hp_mobs = []
+high_hp_mobs = []
+
+for mob in mobs_info:
+    if mob["health"] < 10000:
+        low_hp_mobs.append(mob)
+    else:
+        high_hp_mobs.append(mob)
+
+
+class Context:
+    DEFAULT_MOB: Final[dict] = {"defense": 1000}
+    mob: dict
+
+    def __init__(self, mob: dict | None = None) -> None:
+        if mob is None:
+            self.mob = self.DEFAULT_MOB
+        else:
+            self.mob = mob
+
+
+class AbstractPerson(ABC):
+    lvl: int
+    stat: int
+    buffs: int
+    real_stat: int
+    weapon_atk: int
+
+    min_raw_damage: float
+    max_raw_damage: float
+    max_raw_crit_damage: float
+
+    min_damage: float
+    max_damage: float
+    max_crit_damage: float
+
+    normal_accuracy: int
+    crit_accuracy: int
+    accuracy: float
+
+    average_damage: int
+
+    ctx: Context
+
+    def __init__(self, lvl: int, stat: int, buffs: int, weapon_atk: int, ctx: Context) -> None:
+        self.lvl = lvl
+        self.stat = stat
+        self.buffs = buffs
+        self.weapon_atk = weapon_atk
+
+        self.ctx = ctx
